ce27f433fafdfd3b018f9ddfa3eb3e2cc0c0e687
SulamitaR <111604686+SulamitaR@users.noreply.github.com>
2022-09-28 21:12:49 -0300

    alteração plano de fundo

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function switchTheme() {
     const botao = document.querySelector("#flexSwitchCheckDefault")
 
-    if(botao.checked){
+    if(!botao.checked){
         document.body.style.backgroundColor = '#1a1a1a'
         document.body.style.color = '#fff'
     } else {
         document.body.style.backgroundColor = '#fff'
-        document.body.style.color = '#000'
+        document.body.style.color ='#1a1a1a'
+
     }
 }
